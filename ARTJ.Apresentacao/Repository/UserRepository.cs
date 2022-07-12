862b82c0d2b7f36542d25391cd189d78daebac38
LauraVoxData <lauravoxdata@gmail.com>
2022-07-12 12:37:21 -0300

    Implementando model e repository user

---
+++ ARTJ.Apresentacao/Repository/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARTJ.Apresentacao.Repository
+{
+    public class UserRepository
+    {
+        public static Model.User Get(string userName, string password)
+        {
+            var users = new List<Model.User>
+            {
+                new Model.User {Id = 1, UserName = "Laura", Password = "124", Role = "manager"},
+                new Model.User {Id = 2, UserName = "Suzany", Password = "123", Role = "employee"}
+        };
+            return (Model.User)users.FirstOrDefault(x => x.UserName.ToLower() == userName.ToLower() && x.Password == password);
+        }
+    }
+}
